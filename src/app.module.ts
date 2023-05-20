bb8458121a8bd2025cbe626c0d6a5205d8a575c9
Moi1oM <hcloud0806@gmail.com>
2023-05-20 22:42:56 +0900

    [DB connection]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './commons/common/logger/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DBNAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SuccessInterceptor } from './commons/common/interceptors/success.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './commons/common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //CORS Setting
+  app.enableCors();
+
+  //SuccessInterceptor Setting
+  app.useGlobalInterceptors(new SuccessInterceptor());
+
+  //Validation Setting
+  app.useGlobalPipes(new ValidationPipe());
+
+  //Filter Setting
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Swagger Setting
+  const config = new DocumentBuilder()
+    .setTitle('Novelier Docs')
+    .setDescription('Novelier 0.1 API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
